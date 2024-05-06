81598026d8b7d9d271f5a7906b2ac7e17ff185a6
suraj subedi <surajsubedi059@gmail.com>
2024-05-06 23:05:18 +0545

    feat: navbar added and design improved

--- src/components/static/Navbar.tsx
+++ src/components/static/Navbar.tsx
@@ -1,36 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { FavouritesContext } from "../../sotre/context/FavouritesContext";
 
+const Navbar = ({ children }: { children: React.ReactNode }) => {
+  const navLinks = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "Favourites",
+      link: "/favourites",
+    },
+  ];
 
-const Navbar = ({children} : {children  : React.ReactNode}) => {
-    const navLinks = [
-        {
-            title : "Home",
-            link : "/"
-        },
-        {
-            title : "Favourites",
-            link : "/favourites"
-        },
-        ]
+  const { favourites } = useContext(FavouritesContext);
+
   return (
     <div className="space-y-6 max-w-7xl mx-auto p-10">
-    <div className="flex sticky top-2  items-center justify-center mx-auto py-2 px-10 gap-5 border border-slate-300 rounded-3xl  w-fit  bg-slate-900 backdrop-blur-md backdrop-brightness-90 ">
-        {
-            Array.isArray(navLinks) && 
-            navLinks.map((item , index) => (
-                <NavLink to={item.link} key={index}  >
-                    {item.title}
-                </NavLink>
-            ))
-        }
+      <div className="flex sticky top-2  items-center justify-center mx-auto py-2 px-10 gap-5 border border-slate-300 rounded-3xl  w-fit  bg-slate-900 backdrop-blur-md backdrop-brightness-90 ">
+        {Array.isArray(navLinks) &&
+          navLinks.map((item, index) => (
+            <div className="relative">
+            <NavLink
+              to={item.link}
+              key={index}
+              className={({ isActive }) =>
+                isActive
+                  ? "font-bold text-slate-50"
+                  : " font-medium text-slate-300"
+              }
+            >
+              {item.title}
+              {
+                item.title === "Favourites" &&
+              <p className="absolute top-4 -right-4 text-[12px] bg-red-500 text-slate-300 rounded-full size-4 flex items-center justify-center ">{favourites.length }</p>
+              }
+            </NavLink>
+            </div>
+          ))}
+      </div>
+      {children}
     </div>
-    {
-        children
-    }
+  );
+};
 
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
